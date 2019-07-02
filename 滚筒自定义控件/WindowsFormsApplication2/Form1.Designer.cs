400bdbfb27d81d1a758240fe329499fcd83b430f
luoplk <1741762704@qq.com>
2019-07-02 10:09:12 +0800

    123

---
+++ 滚筒自定义控件/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new WindowsFormsControlLibrary2.UserControl1();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.Angle = 21966F;
+            this.userControl11.Location = new System.Drawing.Point(33, 93);
+            this.userControl11.Margin = new System.Windows.Forms.Padding(5);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.RotateSpd = 15F;
+            this.userControl11.RotateState = WindowsFormsControlLibrary2.ROTATE_STATE.ForwardRotating;
+            this.userControl11.Size = new System.Drawing.Size(587, 575);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.WaterLevel = 54.90000000000051D;
+            this.userControl11.WaterState = WindowsFormsControlLibrary2.WATER_STATE.Stop;
+            this.userControl11.Load += new System.EventHandler(this.userControl11_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 726);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userControl11);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WindowsFormsControlLibrary2.UserControl1 userControl11;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 滚筒自定义控件/WindowsFormsControlLibrary2/UserControl1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary2
+{
+    public enum WATER_STATE { Stop, In, Out };
+    public enum ROTATE_STATE { Stop, ForwardRotating, ReversaRotating };
+
+
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                        ControlStyles.ResizeRedraw |
+                        ControlStyles.AllPaintingInWmPaint, true);  
+
+        }
+
+        private double waterLevel;
+
+        public double WaterLevel
+        {
+            get { return waterLevel; }
+            set { waterLevel = value; this.Refresh(); }
+        }
+
+        private WATER_STATE waterState;
+
+        public WATER_STATE WaterState
+        {
+            get { return waterState; }
+            set { waterState = value; this.Refresh(); }
+        }
+
+        private ROTATE_STATE rotateState;
+
+        public ROTATE_STATE RotateState
+        {
+            get { return rotateState; }
+            set { rotateState = value; this.Refresh(); }
+        }
+
+
+        float angle;
+
+        public float Angle
+        {
+            get { return angle; }
+            set { angle = value; }
+        }
+
+        float rotateSpd = 3;
+
+        public float RotateSpd
+        {
+            get { return rotateSpd; }
+            set { rotateSpd = value; }
+        }
+        
+
+        private void UserControl1_Paint(object sender, PaintEventArgs e)
+        {
+            //画边框
+            Graphics g = e.Graphics;
+            g.DrawRectangle(new Pen(Color.Black, 3), 0,-3,this.Width-3,this.Height);
+
+            //画变化的水位
+            int waterHeight = (int)(waterLevel / 100.0 * this.Height);
+            Rectangle waterRect = new Rectangle(2,this.Height -waterHeight -4 , this.Width - 6, waterHeight);
+            g.FillRectangle(Brushes.SkyBlue, waterRect);
+
+            //画旋转的滚筒
+            Image imgFans = Properties.Resources.fans3;
+
+
+            int xPos = (this.Width - imgFans.Width)/2 ;
+            int yPos = (this.Height - imgFans.Height) / 2;
+
+            Point imgRotateCenterPos = new Point(imgFans.Width/2,imgFans.Height/2); //旋转中心在图片坐标(相对于图片本身)
+            Rectangle rcShow = new Rectangle(xPos, yPos, imgFans.Width,imgFans.Height);　//图片要绘制的位置区域.
+            
+            //把 相对于图片的旋转中心坐标  转换为  绘制区域的坐标
+            PointF centerPos = new Point(imgRotateCenterPos.X+rcShow.Left, imgRotateCenterPos.Y+rcShow.Top);
+            
+            g.TranslateTransform(centerPos.X,centerPos.Y); //源点移动到旋转中心
+            g.RotateTransform(angle); //旋转
+            g.TranslateTransform(-centerPos.X, -centerPos.Y);//还原源点
+            
+            //在某个起点显示图像
+            g.DrawImage(imgFans, rcShow.Left,rcShow.Top,rcShow.Width, rcShow.Height);
+
+        }
+
+     
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (WaterState == WATER_STATE.In)
+            {
+                WaterLevel += 0.1;
+
+                if (WaterLevel >= 100.0)
+                {
+                    WaterLevel = 100.0;
+                }
+            }
+
+            if (WaterState == WATER_STATE.Out)
+            {
+                WaterLevel -= 0.1;
+
+                if (WaterLevel <= 0.0)
+                {
+                    WaterLevel = 0.0;
+                }
+            }
+
+            if (RotateState == ROTATE_STATE.ForwardRotating)
+            {
+                Angle += rotateSpd;
+            }
+            if (RotateState == ROTATE_STATE.ReversaRotating)
+            {
+                Angle -= rotateSpd;
+            }
+
+
+        }
+    }
+}
